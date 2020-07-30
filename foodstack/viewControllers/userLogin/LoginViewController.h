5d0c2154596f7eeb30b302e8e1898f94940ca3e6
Angel Gutierrez <41581996+anggutierrez@users.noreply.github.com>
2020-07-30 10:26:02 -0700

    Update LoginViewController.h

--- foodstack/viewControllers/userLogin/LoginViewController.h
+++ foodstack/viewControllers/userLogin/LoginViewController.h
@@ -1,1 +1,17 @@
+//
+//  LoginViewController.h
+//  foodstack
+//
+//  Created by Angel Gutierrez on 7/14/20.
+//  Copyright © 2020 Angel Gutierrez. All rights reserved.
+//
 
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
