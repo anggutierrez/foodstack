cb7c651cdbab6c580317ecc82647b7b0f579fecc
Angel Gutierrez <41581996+anggutierrez@users.noreply.github.com>
2020-07-30 10:29:24 -0700

    Update ComposeRecipeViewController.h

--- foodstack/viewControllers/composeViews/ComposeRecipeViewController.h
+++ foodstack/viewControllers/composeViews/ComposeRecipeViewController.h
@@ -1,1 +1,17 @@
+//
+//  ComposeRecipeViewController.h
+//  foodstack
+//
+//  Created by Angel Gutierrez on 7/17/20.
+//  Copyright © 2020 Angel Gutierrez. All rights reserved.
+//
 
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeRecipeViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
