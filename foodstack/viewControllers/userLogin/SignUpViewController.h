c9d3346f178836f360ea574715744bf723d25021
Angel Gutierrez <41581996+anggutierrez@users.noreply.github.com>
2020-07-30 10:26:35 -0700

    Update SignUpViewController.h

--- foodstack/viewControllers/userLogin/SignUpViewController.h
+++ foodstack/viewControllers/userLogin/SignUpViewController.h
@@ -1,1 +1,17 @@
+//
+//  SignUpViewController.h
+//  foodstack
+//
+//  Created by Angel Gutierrez on 7/15/20.
+//  Copyright © 2020 Angel Gutierrez. All rights reserved.
+//
 
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignUpViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
